6ad82ca5ec89b1c835b650db732584bbf906c4a1
Ivan Cherepanov <iamfrontender@gmail.com>
2016-12-07 20:11:24 +0300

    fixed help output

--- bin/index.js
+++ bin/index.js
@@ -17,28 +17,29 @@ program.on('--help', () => {
   console.log(chalk.red('AVAILABLE COMMANDS'), '///');
   console.log(`
     Syntax ${chalk.red('///')}
-      evt-auth <cmd> [options...]
+      evt-config <cmd> [options...]
       
     Commands ${chalk.red('///')}
       add      adds new field to configuration store (account)
-        ${chalk.grey('evt-auth add QA-42')} 
-        ${chalk.grey('evt-auth add "My Account"')} 
+        ${chalk.grey('evt-config add QA-42')} 
+        ${chalk.grey('evt-config add "My Account"')} 
         
       list     lists all available configuration fields
-        ${chalk.grey('evt-auth list')}         
+        ${chalk.grey('evt-config list')}         
         
       set      sets the given value by given path in store
-        ${chalk.grey('evt-auth set QA6.authorization i65DFShdfgdfg523FDst')} 
-        ${chalk.grey('evt-auth set gist.token 5RdsY6Wt745T953TqqweT53')}
-        ${chalk.grey('evt-auth set main.apiUrl https://api-eu.evrythng.com')}
-        ${chalk.grey('evt-auth set user.name "Dashe Avenhrust"')}
+        ${chalk.grey('evt-config set QA6.authorization i65DFShdfgdfg523FDst')} 
+        ${chalk.grey('evt-config set gist.token 5RdsY6Wt745T953TqqweT53')}
+        ${chalk.grey('evt-config set main.apiUrl https://api-eu.evrythng.com')}
+        ${chalk.grey('evt-config set user.name "Dashe Avenhrust"')}
         
       get      receives the configuration located by given path in store
-        ${chalk.grey('evt-auth get QA6')}        
-        ${chalk.grey('evt-auth get valut.roles.0')}
-                
-                
-  `)
+        ${chalk.grey('evt-config get QA6')}        
+        ${chalk.grey('evt-config get valut.roles.0')}
+        
+      delete   will completely remove the given key from configuration
+        ${chalk.grey('evt-config delete user.name')}
+  `);
 });
 
 program
@@ -55,7 +56,7 @@ function run(cmd, opts) {
   if (isDefined(cmd)) {
     if (isExist(cmd)) {
       execute(cmd, opts)
-        .catch(err => exit(err));
+        .catch(exit);
     } else {
       exit(`Command not supported: ${cmd}`);
     }
